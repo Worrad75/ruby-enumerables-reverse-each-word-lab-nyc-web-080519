6a535d89ee358c48a12c53ac83d656f2a87bc16c
Darrow Palast <dpalast@gmail.com>
2019-08-03 01:34:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,6 +15,6 @@ def reverse_each_word(str)
     reversed_sentance << backwards
   end
   
-  reversed_sentance.join(" ")
+  reversed_sentance = reversed_sentance.join(" ")
   reversed_sentance
 end
